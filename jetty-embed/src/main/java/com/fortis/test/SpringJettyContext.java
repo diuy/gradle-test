0292bffbad14aac60fd9502de6d88bf0adcdcc52
xiachao <xiachao@cdfortis.com>
2018-07-23 14:46:29 +0800

    111

--- jetty-embed/src/main/java/com/fortis/test/SpringJettyContext.java
+++ jetty-embed/src/main/java/com/fortis/test/SpringJettyContext.java
@@ -4,8 +4,11 @@ import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.util.resource.Resource;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
 
 public class SpringJettyContext implements JettyContext {
 
@@ -31,7 +34,6 @@ public class SpringJettyContext implements JettyContext {
         ServletContextHandler handler = new ServletContextHandler(enableSessions ? ServletContextHandler.SESSIONS : ServletContextHandler.NO_SESSIONS);
         handler.setContextPath(contextPath);
         handler.setResourceBase(resourcePath);
-
         ServletHolder servlet = new ServletHolder(DispatcherServlet.class);
         servlet.setInitParameter("contextConfigLocation", servletConfigLocation);
         handler.addServlet(servlet, servletPath);
